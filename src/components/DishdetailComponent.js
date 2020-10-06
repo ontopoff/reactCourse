aab44ca187657095cfd294bb9bf42fddab140214
ontopoff <avenue200@yandex.ru>
2020-10-06 21:06:21 +0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+import {Card, CardBody, CardImg, CardText, CardTitle, BreadcrumbItem, Breadcrumb} from "reactstrap";
+import { Link } from "react-router-dom";
 
-
-     function RenderDish({dish}) {
+function RenderDish({dish}) {
         if(dish != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -28,22 +28,22 @@ import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
 
     function RenderComments({comments}) {
         if (comments != null) {
-            const comment = comments.map((comment) => {
-                return (
-                    <div key={comment.id}>
-                        <li>{comment.comment}</li>
-                        <br></br>
-                        <li>-- {comment.author} , {new Intl.DateTimeFormat('en-US',{ year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                        <br></br>
-                    </div>
-                )
-            });
-
-            return (
+            return(
                 <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        <h4>Comments</h4>
-                        {comment}
+                        {comments.map((comment) => {
+                            return(
+                              <li key={comment.id}>
+                                  <p>
+                                      {comment.comment}
+                                  </p>
+                                  <p>
+                                      -- {comment.author} , {new Intl.DateTimeFormat('en-US',{ year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                  </p>
+                              </li>
+                            );
+                        })}
                     </ul>
                 </div>
             );
@@ -56,14 +56,27 @@ import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
     };
 
     const DishDetail = (props) => {
-        const dish = props.dish;
-        if (dish != null) {
+        if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
-                            <RenderDish dish ={dish}/>
-                            <RenderComments comments={dish.comments} />
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to='/menu'>Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr/>
                         </div>
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={props.dish}/>
+                        <RenderComments comments={props.comments} />
+                    </div>
                 </div>
             );
         }
