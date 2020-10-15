cef52c64dfd315e579333fa3be96b5b7dfe35d7d
ontopoff <avenue200@yandex.ru>
2020-10-15 16:59:52 +0300

    React-Redux-Form Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,21 +1,26 @@
-import { createStore, combineReducers ,applyMiddleware} from "redux";
-import { Dishes} from "./dishes";
-import { Promotions} from "./promotions";
-import { Comments} from "./comments";
-import { Leaders} from "./leaders";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import {createForms} from "react-redux-form";
+import {Dishes} from "./dishes";
+import {Promotions} from "./promotions";
+import {Comments} from "./comments";
+import {Leaders} from "./leaders";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import {InitialFeedback} from "./forms";
 
 export const ConfigureStore = () => {
-  const store = createStore(
-   combineReducers({
-     dishes: Dishes,
-     comments: Comments,
-     promotions: Promotions,
-     leaders: Leaders
-   }),
-      applyMiddleware(thunk, logger)
-  );
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
+        }),
+        applyMiddleware(thunk, logger)
+    );
 
-  return store;
+    return store;
 };
